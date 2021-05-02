4887989a7053f80f118e5b3d91dca446a242d097
David Kamholz <lautgesetz@gmail.com>
2021-05-02 14:49:59 -0700

    fix last commit

--- perllib/Lexicon/Importer.pm
+++ perllib/Lexicon/Importer.pm
@@ -68,9 +68,9 @@ EOF
         $seen_record_ids{$entry->{record}} = $record_id;
       }
 
-      my $entry_id = select_single($db, <<'EOF', $source_id, map({ ensure_nfc($entry->{$_}) } qw/headword headword_normalized pos root page_num/), $record_id);
-INSERT INTO entry (source_id, headword, headword_normalized, pos, root, page_num, record_id)
-VALUES (?, ?, ?, ?, ?, ?, ?)
+      my $entry_id = select_single($db, <<'EOF', $source_id, map({ ensure_nfc($entry->{$_}) } qw/headword headword_normalized pos root/), $record_id);
+INSERT INTO entry (source_id, headword, headword_normalized, pos, root, record_id)
+VALUES (?, ?, ?, ?, ?, ?)
 RETURNING id
 EOF
 
