21822841f63867bc2ffd9ff1c8419a2c9814dc2a
David Kamholz <lautgesetz@gmail.com>
2021-05-05 12:00:29 -0700

    comment out socket code for now

--- app/src/routes/$layout.svelte
+++ app/src/routes/$layout.svelte
@@ -3,17 +3,17 @@
   import Icon from 'svelte-awesome';
   import Login from '$components/Login.svelte';
   import NavBar from '$components/NavBar.svelte';
-  import { browser } from '$app/env';
+  // import { browser } from '$app/env';
   import { faSpinner } from '@fortawesome/free-solid-svg-icons';
   import { fade } from 'svelte/transition';
-  import { io } from "socket.io-client";
+  // import { io } from "socket.io-client";
   import { navigating, page, session } from '$app/stores';
   import { pageLoading } from '$stores';
 
-  if (browser) {
-    const socket = io(import.meta.env.WEBSOCKET);
-    setInterval(() => socket.send('hello'), 1000);
-  }
+  // if (browser) {
+  //   const socket = io(import.meta.env.WEBSOCKET);
+  //   setInterval(() => socket.send('hello'), 1000);
+  // }
 </script>
 
 <svelte:head>
