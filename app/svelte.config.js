a2f48a52d6f5f453ab48664abd267503dd57d25c
David Kamholz <lautgesetz@gmail.com>
2021-07-07 11:36:25 -0500

    Revert "remove redirect in hooks until can figure out how to do it correctly"
    This reverts commit 71adcfc3fab1e618cbcd727bc50b6a6999d3d333.

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -18,6 +18,10 @@ const config = {
     // specifying a different adapter
     adapter: node(),
 
+    prerender: {
+      enabled: false,
+    },
+
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
 
