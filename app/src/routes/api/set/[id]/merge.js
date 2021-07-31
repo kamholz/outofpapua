1a8f1beb8c9acc4bfc5c9384f2c95591292c285a
David Kamholz <lautgesetz@gmail.com>
2021-07-31 00:11:08 -0700

    add set merge endpoint

---
+++ app/src/routes/api/set/[id]/merge.js
@@ -0,0 +1,28 @@
+import errors from '$lib/errors';
+import { arrayCmp, sendPgError, transaction } from '$lib/db';
+import { getFilteredParams, isIdArray } from '$lib/util';
+import { requireAuth } from '$lib/auth';
+
+const allowed = new Set(['sets']);
+const required = new Set(['sets']);
+
+export const post = requireAuth(async ({ body, locals, params }) => {
+  const updateParams = getFilteredParams(body, allowed);
+  if (Object.keys(updateParams).length !== required.size) {
+    return { status: 400, body: { error: errors.missing } };
+  }
+  if (!isIdArray(updateParams.sets)) {
+    return { status: 400, body: { error: '"sets" parameter must be an array of set ids' } };
+  }
+  try {
+    await transaction(locals, (trx) =>
+      trx('set_member')
+      .update({ set_id: Number(params.id) })
+      .where('set_id', arrayCmp(updateParams.sets))
+    );
+    return { body: '' };
+  } catch (e) {
+    console.log(e);
+    return sendPgError(e);
+  }
+});
