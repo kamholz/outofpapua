387f8573448fcdbd32192b8e7b6a99c02b325132
David Kamholz <lautgesetz@gmail.com>
2021-09-09 01:01:15 -0700

    add ipa_conversion_rule endpoint

--- app/src/routes/api/ipa_conversion_rule/index.json.js
+++ app/src/routes/api/ipa_conversion_rule/index.json.js
@@ -1,31 +1,26 @@
-import { arrayCmp, filterPublicSources, knex } from '$lib/db';
-import { getFilteredParams, isIdArray, normalizeQuery, parseArrayNumParams } from '$lib/util';
+import { arrayCmp, knex } from '$lib/db';
+import { getFilteredParams, normalizeQuery, parseArrayParams } from '$lib/util';
 import { pageMax } from '$lib/preferences';
 
-const allowed = new Set(['ids']);
-const arrayNumParams = new Set(['ids']);
+const allowed = new Set(['names']);
+const arrayParams = new Set(['names']);
 
-export async function get({ locals, query }) {
+export async function get({ query }) {
   query = getFilteredParams(normalizeQuery(query), allowed);
   if (!Object.keys(query).length) {
     return { status: 400, body: { error: 'insufficient parameters' } };
   }
-  parseArrayNumParams(query, arrayNumParams);
-  if (!isIdArray(query.ids)) {
-    return { status: 400, body: { error: 'invalid ids parameter' } };
-  }
-  if (query.ids.length > pageMax) {
+  parseArrayParams(query, arrayParams);
+  if (query.names.length > pageMax) {
     return { status: 400, body: { error: `cannot request more than ${pageMax} rules` } };
   }
 
-  const q = knex('source')
-    .where('id', arrayCmp(query.ids))
+  const q = knex('ipa_conversion_rule as icr')
+    .where('name', arrayCmp(query.names))
     .select(
-      'source.id',
-      'source.ipa_conversion_rule',
-      knex.raw('(CASE WHEN source.id IS NOT NULL THEN ipa_conversion_rule_to_javascript(source.ipa_conversion_rule) ELSE NULL END) as javascript_code'),
+      'icr.name',
+      knex.raw('ipa_conversion_rule_to_javascript(icr.name) AS javascript_code')
     );
-  filterPublicSources(q, locals);
 
   return {
     body: {
