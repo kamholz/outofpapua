a73dd116a81ceb49af156b005fade15865a2d865
David Kamholz <lautgesetz@gmail.com>
2021-10-07 13:33:53 -0700

    add custom modal buttons with copy button

--- app/src/components/Modal/Content.svelte
+++ app/src/components/Modal/Content.svelte
@@ -1,13 +1,13 @@
 <script>
   import SetExport from '$components/Modal/SetExport.svelte';
-  import SetExportButtons from '$components/Modal/SetExport.svelte';
+  import SetExportButtons from '$components/Modal/SetExportButtons.svelte';
   import { getContext } from 'svelte';
   import { modal } from '$lib/stores';
   const { open } = getContext('simple-modal');
   $modal = { setExport };
 
   export function setExport(props) {
-    open(SetExport, props);
+    open(SetExport, props, { closeButton: SetExportButtons });
   }
 </script>
 
---
+++ app/src/components/Modal/SetExportButtons.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Icon from 'svelte-awesome';
+  import { faClone, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+  export let onClose;
+  const scale = 1.25;
+  let div;
+
+  function copy() {
+    const content = div.nextElementSibling;
+    const range = document.createRange();
+    range.selectNodeContents(content);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+    document.execCommand("copy");
+    sel.removeAllRanges();
+  }
+</script>
+
+<div bind:this={div}>
+  <span title="Copy" on:click={copy}>
+    <Icon data={faClone} {scale} />
+  </span>
+  <span title="Close" on:click={onClose}>
+    <Icon data={faTimes} {scale} />
+  </span>
+</div>
+
+<style lang="scss">
+  div {
+    display: flex;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    z-index: 999;
+    background-color: white;
+
+    > span {
+      margin-inline-start: 5px;
+    }
+  }
+</style>
