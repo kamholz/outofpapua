852d9304caa7556368ac3d2add307909d0fc2b5e
David Kamholz <lautgesetz@gmail.com>
2022-10-09 18:48:02 -0700

    roll back checkbox change

--- app/src/components/Form.svelte
+++ app/src/components/Form.svelte
@@ -165,7 +165,7 @@
         .field {
           margin-inline-start: 12px;
           grid-column: 2 / 4;
-          &.narrow, &[type="checkbox"] {
+          &.narrow {
             grid-column: 2;
           }
         }      
