bdb9915ab1de7e907675b24f78d2b06a7acfba05
Maria Zielenbach <maria.zielenbach@gmx.de>
2022-11-01 10:21:36 +0100

    changed language name Soeparno (1977d)

--- dictionaries.pl
+++ dictionaries.pl
@@ -3193,7 +3193,7 @@ our $dict = {
     split => ';',
   },
   'Soeparno (1977d)' => {
-    lang_target => 'mvx',
+    lang_target => 'bhw',
     path => 'Biak dictionary.xlsx',
     parser => 'Spreadsheet',
     sheet => 'Biak Manwor',
