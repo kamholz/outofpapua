f854f48332fbf386f56f8a2fc0ed6bca4b47ceb1
Maria Zielenbach <maria.zielenbach@gmx.de>
2023-02-07 08:47:46 +0100

    updated van der Leeden (nd)

--- dictionaries.pl
+++ dictionaries.pl
@@ -4466,6 +4466,7 @@ our $dict = {
     parser => 'Marker',
     lang_national => 'ind',
     lang_regional => 'pmy',
+    lang_vernacular => 'slz'
     strip => 'to',
     headword_preprocess => sub { s/^ø-//r },
   },
