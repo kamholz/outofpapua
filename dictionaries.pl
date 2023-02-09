0b6fa92bcda473f514958c112f3a1c36ff8201b6
Maria Zielenbach <maria.zielenbach@gmx.de>
2023-02-09 11:27:05 +0100

    updated Heston (2015)

--- dictionaries.pl
+++ dictionaries.pl
@@ -1519,7 +1519,7 @@ our $dict = {
     lang_target => 'Fataluku Central',
     path => 'Heston 2015 Central Fataluku.xlsx',
     parser => 'Spreadsheet',
-    sheet => 'Sheet1'
+    sheet => 'Sheet1',
     columns => [
       [0, 'headword'],
       [1, 'sn'],
