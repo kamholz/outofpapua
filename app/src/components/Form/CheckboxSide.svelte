16b7a4e0b6c3ed06c96c15428bd43f388246faaf
David Kamholz <lautgesetz@gmail.com>
2023-04-02 23:05:57 -0700

    add missing file

---
+++ app/src/components/Form/CheckboxSide.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+  export let label;
+  export let checked;
+</script>
+
+<label>
+  <input
+    type="checkbox"
+    {name}
+    value="1"
+    bind:checked
+  >
+  {label}
+</label>
+
+<style>
+  label {
+    margin-inline: 14px 0;
+  }
+</style>
