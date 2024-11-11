4d2432984f571cfa0f92d3888177c3827dccfe6d
MariaZie <maria.zielenbach@gmx.de>
2024-11-11 10:33:04 +0100

    updated Riesberg, Zöllner & Zöllner (2016)

--- dictionaries.pl
+++ dictionaries.pl
@@ -6717,7 +6717,7 @@ our $dict = {
     lang_target => 'yli',
     path => 'Riesberg_2016_YaliAngguruk.txt',
     parser => 'Marker',
-    skip_marker => ['iv'],
+    skip_marker => ['iv', 'pdr',],
   },
   'Rinnooy (1886)' => {
     lang_target => 'Kisar',
