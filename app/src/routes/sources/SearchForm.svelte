7dff950daa80b111dff0f4804e38a9d9699e87c5
David Kamholz <lautgesetz@gmail.com>
2025-09-07 13:34:42 -0700

    add missing file from last commit

---
+++ app/src/routes/sources/SearchForm.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Form from '$components/Form.svelte';
+  import { getContext } from 'svelte';
+
+  export let query;
+  const langSuggest = getContext('langSuggest');
+  const values = { ...query };
+
+  const fields = [
+    {
+      name: 'lang',
+      label: 'Languages',
+      type: 'suggestMulti',
+      options: langSuggest,
+    },
+  ];
+</script>
+
+<Form
+  {fields}
+  {values}
+  submitLabel="Search"
+  clearable
+  browserSubmit
+  style="--form-width: 40em; --label-width: 23%; --checkbox-width: 5em;"
+/>
